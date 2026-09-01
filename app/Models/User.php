a9a184847ff65dc382651cc0e9be248f13bf49d0
zafirahzaini <zafirahzainiii@gmail.com>
2026-09-01 17:28:09 +0800

    Fix password hashing collision in User model and seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,14 @@
 
 namespace App\Models;
 
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'userID',
         'name',
@@ -28,21 +21,11 @@ class User extends Authenticatable
         'available_status',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
